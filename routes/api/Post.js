c88061ddd9bde4cf11a31a440afbcadbe7a74f1d
Alan Wen <alanjameswen@outlook.com>
2022-03-03 12:16:46 -0800

    Post get, like, delete endpoints

--- routes/api/Post.js
+++ routes/api/Post.js
@@ -1,31 +1,45 @@
 import express from "express";
+import {Post} from "../../db.js";
+
 var router = express.Router();
 
-// TODO: Retrieve a post with an ID
+// Retrieve a post with an ID
 router.get("/:postId", (req, res) => {
     // get id from url
-    const id = req.params.postId;
-    res.send("Get post: " + id);
+    const postId = req.params.postId;
+    const post = await Post.findOne({_id: postId});
+    res.type('json');
+    res.send(post);
 });
 
-// TODO: Like a post
+// Like a post
 router.post("/:postId", (req, res) => {
     const id = req.params.postId;
     const like = req.query.like;
+    const post = await Post.findOne({_id: id});
     if (like) {
         // todo: add like to post
+        post.likes++;
+        await post.save();
+    }
+    else {
+        // todo: dislike post
+        post.dislikes++;
+        await post.save();
     }
 });
 
 // TODO: Creates a new Post
+// Need to interface with front-end to define how body looks like
 router.post("/", (req, res) => {
     const post = req.body;
 });
 
-// TODO: Deletes a post
+// Deletes a post
 router.delete("/:postId", (req, res) => {
     const id = req.params.postId;
-    res.send("Delete post: " + id);
+    const post = await Post.findOne({_id: id});
+    await post.remove();
 });
 
 export default router;
