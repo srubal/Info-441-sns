130eb7b5569f0a5bba3990c7705f17f6d1e6324a
Alan Wen <alanjameswen@outlook.com>
2022-03-03 11:40:54 -0800

    course api endpoints

--- routes/api/Course.js
+++ routes/api/Course.js
@@ -1,9 +1,21 @@
 import express from "express";
 var router = express.Router();
 
-// default course endpoint
-router.get("/", function (req, res) {
-    res.send("Course");
+// TODO: Retrieve a course with an ID
+router.get("/:courseId", (req, res) => {
+    const id = req.params.courseId;
+    res.send("Course ID: " + id);
+});
+
+// TODO: Delete a course with an ID
+router.delete("/:courseId", (req, res) => {
+    const id = req.params.courseId;
+    res.send("Delete course: " + id);
+});
+
+// TODO: Create a course
+router.post("/", (req, res) => {
+    const course = req.body;
 });
 
 export default router;
