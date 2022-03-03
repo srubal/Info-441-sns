7f8e87da723654941571949767fce787444f7a47
Stephan Rubalcava <srubal@uw.eud>
2022-03-03 12:12:12 -0800

    more account implementation + addpost

--- routes/api/Post.js
+++ routes/api/Post.js
@@ -1,9 +1,30 @@
 import express from "express";
+import Hashes from "jshashes";
+import Post, {Account} from "../../db.js";
 var router = express.Router();
 
 // default post endpoint
 router.get("/", function (req, res) {
     res.send("Post");
 });
+
+router.post("/newPost", async (req, res) => {
+    //***code for associating post with user account***
+    var SHA256 = new Hashes.SHA256;
+    let user = req.session.account.username;
+    let accountHash = SHA256.hex(user);
+    let acct = await Account.findOne({emailHash: accountHash});
+    let uid = acct._id;
+    let today = new Date();
+    let date = today.getDay() + (today.getMonth() + 1) + today.getFullYear();
+    let post = new Post({
+                        courseID: req.body.courseID,
+                        title: req.body.title,
+                        content: req.body.content,
+                        likes: [],
+                        dislikes: [],
+                        date: date
+    })
+})
 
 export default router;
--- routes/api/UserActions.js
+++ routes/api/UserActions.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Hashes from "jshashes";
-import {Account} from "../../db.js";
+import Post, {Account} from "../../db.js";
 var router = express.Router();
 
 // default user action endpoint
@@ -11,23 +11,26 @@ router.get("/", function (req, res) {
 //Creates account data by hashing their email, creating a unique anonymous identifier for them
 router.post("/initializeUser", async (req, res) => {
     if(req.session.isAuthenticated && req.session.account.username.includes("@uw.edu")) {
-        var SHA256 = new Hashes.SHA256;
-        let accHash = SHA256.hex(req.session.account.username);
-        let acct = await Account.findOne({emailHash: accHash});
-        //replace role with prompt for user
-        let role = "student";
-        //And if account is not on banned list
-        if(!acct) {
-            let newAccount = new Account({
-                                        permission: "",
-                                        posts: [],
-                                        role: role,
-                                        emailHash: accHash
-                                        });
-            await newAccount.save();
+        try {
+            var SHA256 = new Hashes.SHA256;
+            let accHash = SHA256.hex(req.session.account.username);
+            let acct = await Account.findOne({emailHash: accHash});
+            //replace role with prompt for user
+            let role = "student";
+            //And if account is not on banned list
+            if(!acct) {
+                let newAccount = new Account({
+                                            permission: "",
+                                            role: role,
+                                            emailHash: accHash
+                                            });
+                await newAccount.save();
+            }
+            res.type('json');
+            res.send({status:'success'});
+        }catch (err) {
+
         }
-        res.type('json');
-        res.send({status:'success'});
     }else {
         //redirect to /
     }
@@ -36,9 +39,22 @@ router.post("/initializeUser", async (req, res) => {
 router.get("/account", async (req, res) => {
         let currAccount = req.session.account.username;
         let reqAccount = req.query.account;
-
-})
-router.post("/addPost", async (req, res) => {
-
+        var SHA256 = new Hashes.SHA256;
+        let currAccHash = SHA256.hex(currAccount);
+        let acct = await Account.findOne({emailHash: currAccHash});
+        let reqAcct = await Account.findOne({_id: reqAccount});
+        if(reqAcct.emailHash == currAccHash || acct.permission == "admin") {
+            let posts = await Post.findAll({uid: reqAccount._id})
+            let retObj = {
+                          uid: reqAccount._id,
+                          permission: reqAcct.permission,
+                          role: reqAccount.role,
+                          posts: posts
+            }
+            res.type("JSON");
+            res.send(JSON.stringify(retObj));
+        }else {
+            res.send({status:"error", error:"permission denied"});
+        }
 })
 export default router;
