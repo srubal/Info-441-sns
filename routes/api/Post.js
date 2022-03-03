bd4b40bc1b976db19cffe7c08d106434ac52b56a
Alan Wen <alanjameswen@outlook.com>
2022-03-03 11:38:49 -0800

    index api and post api

--- routes/api/Post.js
+++ routes/api/Post.js
@@ -1,9 +1,31 @@
 import express from "express";
 var router = express.Router();
 
-// default post endpoint
-router.get("/", function (req, res) {
-    res.send("Post");
+// TODO: Retrieve a post with an ID
+router.get("/:postId", (req, res) => {
+    // get id from url
+    const id = req.params.postId;
+    res.send("Get post: " + id);
+});
+
+// TODO: Like a post
+router.post("/:postId", (req, res) => {
+    const id = req.params.postId;
+    const like = req.query.like;
+    if (like) {
+        // todo: add like to post
+    }
+});
+
+// TODO: Creates a new Post
+router.post("/", (req, res) => {
+    const post = req.body;
+});
+
+// TODO: Deletes a post
+router.delete("/:postId", (req, res) => {
+    const id = req.params.postId;
+    res.send("Delete post: " + id);
 });
 
 export default router;
