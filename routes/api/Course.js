22ddc6625597f3684456eee7eea00cce1c9ee2ec
Arjun Aggarwal <arjuna5@uw.edu>
2022-03-09 07:22:35 +0530

    Adding course

--- routes/api/Course.js
+++ routes/api/Course.js
@@ -4,18 +4,33 @@ var router = express.Router();
 // TODO: Retrieve a course with an ID
 router.get("/:courseId", (req, res) => {
     const id = req.params.courseId;
-    res.send("Course ID: " + id);
+    let course = await req.db.User.findById(id)
+    res.json(course)
 });
 
 // TODO: Delete a course with an ID
 router.delete("/:courseId", (req, res) => {
     const id = req.params.courseId;
-    res.send("Delete course: " + id);
+    await req.db.Course.deleteOne({_id: id})
+    res.json({status: "success"})
+    // res.send("Delete course: " + id);
 });
 
 // TODO: Create a course
 router.post("/", (req, res) => {
     const course = req.body;
+    let courseId = course.courseId
+    let title = course.title
+    let content = course.content
+
+    let newCourse = new req.db.Course({
+        courseId: courseId,
+        title: title,
+        content: content
+      })
+
+      await newCourse.save()
+  res.json({status: "success"})
 });
 
 export default router;
