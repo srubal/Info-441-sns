c5488e20e3528b539669f1a897baaf8150cdf58d
Audrey Kho <audreyckho@gmail.com>
2022-03-10 13:59:00 -0800

    page tab styles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,12 +9,16 @@ function postSubmit() {
 }
 
 // Allows user to switch between pages on the navbar (Home & All Courses)
-function showPage(newPage, oldPage) {
+function showPage(newPage, oldPage, otherTabId, thisTabId) {
     let show = document.getElementById(newPage);
     let hide = document.getElementById(oldPage);
+    let unselectedTab = document.getElementById(otherTabId);
+    let selectedTab = document.getElementById(thisTabId);
 
     if (!hide.classList.contains("hidden")) {
         hide.classList.add("hidden");
         show.classList.remove("hidden");
+        selectedTab.classList.add("selected");
+        unselectedTab.classList.remove("selected");
     }
 }
